3d0c6e9c166f423433bf6b155df5908da21d0435
Megan Coppins <meganlcoppins@gmail.com>
2020-03-26 08:00:57 -0700

    get routes commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +16,65 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
+// routing
+// put
+//const res = await fetch("/api/workouts/" + id, {
+    
+// post
+// const res = await fetch("/api/workouts", {
+// app.post("/api/workouts", ({ body }, res) => {
+//   db.Workout.create(body)
+//     .then(({ _id }) =>
+//       db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true })
+//     )
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// get (find all)
+// res = await fetch("/api/workouts");
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// get
+// fetch("/api/workouts/range")
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/library", (req, res) => {
+  db.Library.find({})
+    .then(dbLibrary => {
+      res.json(dbLibrary);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // db.Library.create({ name: "Campus Library" })
 //   .then(dbLibrary => {
@@ -24,37 +83,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 //   .catch(({message}) => {
 //     console.log(message);
 //   });
-
-// app.post("/submit", ({body}, res) => {
-//   db.Book.create(body)
-//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/books", (req, res) => {
-//   db.Book.find({})
-//     .then(dbBook => {
-//       res.json(dbBook);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/library", (req, res) => {
-//   db.Library.find({})
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
 
 // app.get("/populated", (req, res) => {
 //   db.Library.find({})
