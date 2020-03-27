cb44d3c388f62e14af9ba87f6197d93f053ddbf9
Megan Coppins <meganlcoppins@gmail.com>
2020-03-26 19:05:00 -0700

    corrected model commit

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: String,
-  exercises: String
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: {
+    type: String,
+    name: String,
+    weight: Number,
+    sets: Number,
+    reps: Number,
+    duration: Number
+  }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
